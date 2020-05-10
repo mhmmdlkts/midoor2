1e19d5924cb8072a514d96ffe6bf4f8bf0c4e94f
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-05-10 15:21:10 +0200

    Unused methods removed

--- Assets/Scripts/SpawnPoints/ct/midRightSpawn.cs
+++ Assets/Scripts/SpawnPoints/ct/midRightSpawn.cs
@@ -8,13 +8,7 @@ public class midRightSpawn : MonoBehaviour
     bool isGoingPick = false, isGoingHide = false;
 
     private float speed = 1.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (isGoingPick)
