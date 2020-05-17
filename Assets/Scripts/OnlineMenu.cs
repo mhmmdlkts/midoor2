d4e26ef0e30fe474c6d45b0bafa99c36ab8e0984
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-05-17 19:54:10 +0200

    Ready for online

---
+++ Assets/Scripts/OnlineMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OnlineMenu : MonoBehaviour
+{
+    public GameObject pp, name, money, wins, rank;
+    public Sprite[] rankList;
+    void Start()
+    {
+        setStatus();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void setStatus()
+    {
+        wins.GetComponent<Text>().text = PlayerPrefs.GetInt("total_wins",0) + "";
+        money.GetComponent<Text>().text = "$" + PlayerPrefs.GetInt("money",0);
+        rank.GetComponent<Image>().sprite = rankList[PlayerPrefs.GetInt("rank", 4)];
+        name.GetComponent<Text>().text = PlayerPrefs.GetString("name", "Mali");
+    }
+    
+    public void button_ranked_room()
+    {
+        
+    }
+    
+    public void button_join_room()
+    {
+        
+    }
+    
+    public void button_create_room()
+    {
+        
+    }
+}
